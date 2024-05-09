e17a83bd29bbb7bcb14efcc097b23f5380c1aebe
anik.malik@nntsoftware.in <anik.malik@nntsoftware.in>
2024-05-09 12:04:20 +0530

    chore: Update imports and lazy load RotatingCube component in HeroSection

--- src/app/(home)/components/HeroSection.tsx
+++ src/app/(home)/components/HeroSection.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 import React from "react";
 import Title from "./Title";
 import Spline from "@splinetool/react-spline";
-import RotatingCube from "@/components/rotating-cube";
+import dynamic from "next/dynamic";
+
+// import RotatingCube from "@/components/rotating-cube";
+const RotatingCube = dynamic(() => import('@/components/rotating-cube'), { ssr: false });
 
 export default function HeroSection() {
   return (
@@ -46,7 +49,7 @@ export default function HeroSection() {
         {/* </div>  */}
         {/* <Spline scene="https://prod.spline.design/7a2ZWwrf3jXU5WMw/scene.splinecode" /> */}
         {/* <div className=" object-contain"> */}
-        <div className="lg:absolute top-0 left-0 right-0 bottom-0 flex lg:justify-end items-center lg:ml-72">
+        <div className="lg:absolute top-0 left-0 right-0 bottom-0 flex lg:justify-end items-center lg:ml-72 transform">
           <RotatingCube />
         </div>
  {/* </div> */}
--- src/components/brain.tsx
+++ src/components/brain.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import Link from "next/link";
 import { motion, useTransform, MotionValue } from "framer-motion";
 
