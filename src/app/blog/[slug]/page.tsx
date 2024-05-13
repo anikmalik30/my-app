23c4f90766168f130ad13020da2bc0609ec05314
anik.malik@nntsoftware.in <anik.malik@nntsoftware.in>
2024-05-13 17:34:01 +0530

    refactor: Add revalidate constant for blog pages

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,6 +5,8 @@ import withFooter from "@/app/withFooter";
 import { PortableText } from "next-sanity";
 import Image from "next/image";
 
+export const revalidate = 30;
+
 async function getData(slug: string) {
   const query = `
     *[_type == "blog" && slug.current == '${slug}']{
