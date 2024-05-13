cbe3e2877ca5aeaf01250da97893de285563262a
anik.malik@nntsoftware.in <anik.malik@nntsoftware.in>
2024-05-13 16:55:42 +0530

    refactor: Update withFooter component to include display name

--- src/app/withFooter.tsx
+++ src/app/withFooter.tsx
@@ -3,12 +3,19 @@ import React from 'react';
 import Footer from './(home)/components/Footer';
 
 const withFooter = (Component:any) => {
-  return (props:any) => (
+  const WithFooter = (props:any) => (
     <>
       <Component {...props} />
       <Footer />
     </>
   );
+
+  WithFooter.displayName = `WithFooter(${getDisplayName(Component)})`;
+
+  return WithFooter;
 };
 
+function getDisplayName(Component: React.ComponentType<any>) {
+  return Component.displayName || Component.name || 'Component';
+}
 export default withFooter;
