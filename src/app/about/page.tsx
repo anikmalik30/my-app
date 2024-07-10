e75aff6797209d81ca192328ee1a2a3682e69d4e
anik.malik@nntsoftware.in <anik.malik@nntsoftware.in>
2024-07-10 15:10:24 +0530

    refactor: Update RotatingCube component and add ScrollSvg component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,8 @@ import Header from "../(home)/components/Header";
 import withFooter from "../withFooter";
 // import AAnimation from "@/components/a-animation";
 import dynamic from "next/dynamic";
-const AAnimation = dynamic(() => import("@/components/a-animation"), { ssr: false });
+import ScrollSvg from "@/components/ui/ScrollSvg"
+// const AAnimation = dynamic(() => import("@/components/a-animation"), { ssr: false });
 
 
 function AboutPage() {
@@ -86,32 +87,7 @@ function AboutPage() {
                   <Image src="/sign.svg" alt="Description of image" width={140} height={140} />
                 </div>
                 {/* SCROLL SVG*/}
-                <motion.svg
-                  initial={{ opacity: 0.2, y: 0 }}
-                  animate={{ opacity: 1, y: "10px" }}
-                  transition={{
-                    repeat: Infinity,
-                    duration: 3,
-                    ease: "easeInOut",
-                  }}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={50}
-                  height={50}
-                >
-                  <path
-                    d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-                    stroke="#ffffff"
-                    strokeWidth="1"
-                  ></path>
-                  <path d="M12 6V14" stroke="#ffffff" strokeWidth="1"></path>
-                  <path
-                    d="M15 11L12 14L9 11"
-                    stroke="#ffffff"
-                    strokeWidth="1"
-                  ></path>
-                </motion.svg>
+                <ScrollSvg/>
               </div>
               {/* SKILLS CONTAINER */}
               <div
@@ -142,32 +118,7 @@ function AboutPage() {
                   })}
                 </motion.div>
                 {/* SKILL SCROLL */}
-                <motion.svg
-                  initial={{ opacity: 0.2, y: 0 }}
-                  animate={{ opacity: 1, y: "10px" }}
-                  transition={{
-                    repeat: Infinity,
-                    duration: 3,
-                    ease: "easeInOut",
-                  }}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={50}
-                  height={50}
-                >
-                  <path
-                    d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-                    stroke="#ffffff"
-                    strokeWidth="1"
-                  ></path>
-                  <path d="M12 6V14" stroke="#ffffff" strokeWidth="1"></path>
-                  <path
-                    d="M15 11L12 14L9 11"
-                    stroke="#ffffff"
-                    strokeWidth="1"
-                  ></path>
-                </motion.svg>
+                <ScrollSvg/>
               </div>
               {/* EXPERIENCE CONTAINER */}
               <div
@@ -288,16 +239,10 @@ function AboutPage() {
               </div>
             </div>
             {/* SVG CONTAINER */}
-            {/* <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
-            <Brain scrollYProgress={scrollYProgress} />
-          </div> */}
-            {/* <div className="hidden lg:block w-1/3 sticky z-30 xl:w-1/2"> */}
-            {/* </div> */}
-            {/* <AAnimation /> */}
           </div>
 
         </motion.div>
-        <AAnimation />
+        {/* <AAnimation /> */}
       {/* <div className="my-10 -z-10"> */}
       {/* </div> */}
       </div>
--- src/components/rotating-cube.tsx
+++ src/components/rotating-cube.tsx
@@ -1,16 +1,10 @@
 "use client";
 import Spline from "@splinetool/react-spline";
-import React, { useRef, useEffect } from "react";
-// import * as THREE from "three";
-// import { Reflector } from "three/examples/jsm/objects/Reflector.js";
+import React from "react";
 
 const RotatingCube: React.FC = () => {
 return (
   <div style={{scale:"0.5"}}>
-
-    {/* old one */}
-    {/* <Spline scene="https://prod.spline.design/7a2ZWwrf3jXU5WMw/scene.splinecode"/>  */}
-    {/* New */}
     <Spline scene="https://prod.spline.design/8s57-Maaz4u-3lJe/scene.splinecode" />
   </div>
 );
