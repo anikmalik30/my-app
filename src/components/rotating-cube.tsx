fc5ed3ddc95dcaf7ea2c7c1109ccf53903d402ff
anik.malik@nntsoftware.in <anik.malik@nntsoftware.in>
2024-07-11 16:48:36 +0530

    refactor: Update import path for ScrollSvg component in AboutPage

--- src/components/rotating-cube.tsx
+++ src/components/rotating-cube.tsx
@@ -1,10 +1,15 @@
 "use client";
-import Spline from "@splinetool/react-spline";
-import React from "react";
+import dynamic from 'next/dynamic';
+
+const Spline = dynamic(() => import('@splinetool/react-spline'), {
+  ssr: false, // Disable server-side rendering for this component
+  loading: () => <p>Loading...</p>, // Optional loading placeholder
+});
 
 const RotatingCube: React.FC = () => {
 return (
-  <div style={{scale:"0.5"}}>
+  // <div style={{scale:"0.5"}}>
+  <div style={{ transform: "scale(0.5)" }}>
     <Spline scene="https://prod.spline.design/8s57-Maaz4u-3lJe/scene.splinecode" />
   </div>
 );
