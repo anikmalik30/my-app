c74eeeed41a3b1adbd209caaf8dc00ff696dce55
anik.malik@nntsoftware.in <anik.malik@nntsoftware.in>
2024-08-06 17:14:14 +0530

    refactor: Optimize rendering of components in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,28 +1,30 @@
 import React, { useMemo } from "react";
-import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
 import dynamic from "next/dynamic";
 import withFooter from "../withFooter";
 
-const Skills = dynamic(() => import("./components/Skills"));
-const Projects = dynamic(() => import("./components/Projects"));
+const Header = dynamic(() => import("./components/Header"), { ssr: false });
+const HeroSection = dynamic(() => import("./components/HeroSection"), {
+  ssr: false,
+});
+const Skills = dynamic(() => import("./components/Skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/Projects"), { ssr: false });
 // import ParticlesContainer from "./components/ParticlesContainer";
 
 function Page() {
-  // const header = useMemo(() => <Header />, []);
-  // const heroSection = useMemo(() => <HeroSection />, []);
-  // const skills = useMemo(() => <Skills />, []);
-  // const projects = useMemo(() => <Projects />, []);
+  const header = useMemo(() => <Header />, []);
+  const heroSection = useMemo(() => <HeroSection />, []);
+  const skills = useMemo(() => <Skills />, []);
+  const projects = useMemo(() => <Projects />, []);
 
   return (
     <div className="min-h-screen bg-black overflow-hidden">
       <div className=" dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
         {/* <ParticlesContainer /> */}
         <div className="max-w-7xl mx-auto sm:p-5">
-          <Header />
-          <HeroSection />
-          {/* {header}
-          {heroSection} */}
+          {/* <Header />
+          <HeroSection /> */}
+          {header}
+          {heroSection}
         </div>
         <div
           className="h-10 bg-gradient-to-t from-black absolute -bottom-5 left-0 
@@ -30,10 +32,10 @@ function Page() {
         ></div>
       </div>
       <div className="max-w-7xl mx-auto p-5">
-        <Skills />
+        {/* <Skills /> */}
         {/* <Projects /> */}
-        {/* {skills}
-        {projects} */}
+        {skills}
+        {projects}
       </div>
     </div>
   );
